69d3fe4847d0ff6a1f0db39b9f5522249e051602
Donald-Alvarez <donaldoctavio0210@gmail.com>
2023-06-29 16:57:54 -0600

    Creacion de la carpeta src y ClimaScreen.js

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
-import { View} from 'react-native'
+import { View, Text } from 'react-native'
 import React from 'react'
-import ClimaScreen from './src/ClimaScreen'
 
 const App = () => {
   return (
     <View>
-      <ClimaScreen />
+      <Text>App</Text>
     </View>
   )
 }
